8e203fc7f04af8a964e7cc9247ea71c736f37d68
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-03-27 23:35:26 +0800

    test grid scale 3

--- local_test/gym_env.py
+++ local_test/gym_env.py
@@ -31,7 +31,7 @@ class SnakeGameEnv(gym.Env):
         }
         self.num_snakes = num_snakes
         self.numteams = num_teams
-        self.scale = 5
+        self.scale = 3
         self.render_mode = render_mode
         self.gs = gs
 
