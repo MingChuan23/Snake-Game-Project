2ad732363a35957e4d33ed39ce58a353fdbef1f0
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-04-10 20:23:49 +0800

    retest 4.1

--- local_test/eval.py
+++ local_test/eval.py
@@ -3,6 +3,7 @@ from gym_env import SnakeGameEnv
 from stable_baselines3 import PPO
 import torch
 import json
+from stable_baselines3.common.monitor import Monitor
 
 # action_map = {
 #     0: 'stay',
@@ -24,35 +25,35 @@ with open("param_configs/eval.json", "r") as f:
 model = PPO.load("models/ppo_snake4.1_2.zip")
 
 # Create a new environment instance for evaluation
-env = SnakeGameEnv(**game_params)
+env = Monitor(SnakeGameEnv(**game_params))
 
 # Evaluate the model
-# rew, std = evaluate_policy(model, env, n_eval_episodes=50, render=True, return_episode_rewards=False, warn=False)
+rew, std = evaluate_policy(model, env, n_eval_episodes=10, render=True, return_episode_rewards=False, warn=True)
 
 
 # For getting the explicit actions probabilities, could be good for data and reporting
-num_episodes = 10
-all_action_probs = []
+# num_episodes = 50
+# all_action_probs = []
 
-for _ in range(num_episodes):
-    obs, _ = env.reset()
-    done = False
+# for _ in range(num_episodes):
+#     obs, _ = env.reset()
+#     done = False
 
-    while not done:
-        with torch.no_grad():
-            tensor_obs, _ = model.policy.obs_to_tensor(obs)
-            action_dist = model.policy.get_distribution(tensor_obs)
-            action_probs = torch.exp(action_dist.distribution.logits)[0].tolist()
-            all_action_probs.append(action_probs)
+#     while not done:
+#         with torch.no_grad():
+#             tensor_obs, _ = model.policy.obs_to_tensor(obs)
+#             action_dist = model.policy.get_distribution(tensor_obs)
+#             action_probs = torch.exp(action_dist.distribution.logits)[0].tolist()
+#             all_action_probs.append(action_probs)
 
-        paired_probs = [(action_map[i], round(prob, ndigits=3)) for i, prob in enumerate(action_probs)]
-        print(f"Action Dist: {paired_probs}")
-        env.render()
-        action, _ = model.predict(obs, deterministic=False)
-        print(f"Action: {action_map[int(action)]}")
-        obs, reward, done,_, _ = env.step(int(action))
-
-
+#         paired_probs = [(action_map[i], round(prob, ndigits=3)) for i, prob in enumerate(action_probs)]
+#         print(f"Action Dist: {paired_probs}")
+#         env.render()
+#         action, _ = model.predict(obs, deterministic=False)
+#         print(f"Action: {action_map[int(action)]}")
+#         obs, reward, done,_, _ = env.step(int(action))
 
 
-# print(f"Mean Reward: {rew:.2f}, Std Reward: {std:.2f}")
+
+
+print(f"Mean Reward: {rew:.2f}, Std Reward: {std:.2f}")
