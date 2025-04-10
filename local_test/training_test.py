502da77e3adbf0e3305a35740fb47833ca5790b9
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-04-10 19:53:04 +0800

    test 2 mil

--- local_test/training_test.py
+++ local_test/training_test.py
@@ -21,6 +21,6 @@ model = PPO('CnnPolicy', vec_env, verbose=True, device='cuda', tensorboard_log=l
 
 
 for i in range(20):
-    model.learn(100000, progress_bar=True, tb_log_name="test", reset_num_timesteps=False)
-    model.save('test.zip')
+    model.learn(100000, progress_bar=True, tb_log_name="ppo_snake4.1_single", reset_num_timesteps=False)
+    model.save('ppo_snake4.1_single.zip')
 
