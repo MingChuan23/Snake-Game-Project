b677fc3c65d246dc37043c8bce60744621ab6a40
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-04-10 16:59:36 +0800

    test staged learning

--- local_test/staged_learning.py
+++ local_test/staged_learning.py
@@ -1,6 +1,27 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from gym_env import SnakeGameEnv
+import json
+import os
 
-
+model_name = "ppo_snake4.0"
 log_dir = "logs"
+config_dir = f"param_configs/{model_name}"
+
+param_list = []
+
+for filename in os.listdir(config_dir):
+    with open(os.path.join(config_dir, filename), "r") as f:
+        param_list.append(json.load(f))
+
+model = None
+for i, params in enumerate(param_list):
+    print(params)
+    vec_env = make_vec_env(lambda: SnakeGameEnv(**params), n_envs=32)
+
+    if not model:
+        model = PPO('CnnPolicy', vec_env, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.0003)
+
+    for i in range(1):
+        model.learn(100000, progress_bar=True, tb_log_name=f"{model_name}_{i}", reset_num_timesteps=False)
+        model.save(f'{model_name}_{i}.zip')
