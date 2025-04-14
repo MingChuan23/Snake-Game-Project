25237f7863950f8b0046a2d03d88b6e0690e2057
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-04-14 15:46:01 +0800

    train 4.8, use ga params

--- local_test/training_test.py
+++ local_test/training_test.py
@@ -3,14 +3,20 @@ from stable_baselines3.common.env_util import make_vec_env
 from gym_env import SnakeGameEnv
 from feature_extractor import CustomCNN
 import json
+import os
 
 
 log_dir = "logs"
-config_dir = "param_configs"
+config_dir = "param_configs/ga_params"
 
-with open(f"{config_dir}/4.7_single.json", "r") as f:
-    game_params = json.load(f)
+param_list = []
 
+for filename in sorted(os.listdir(f"{config_dir}")):
+    with open(os.path.join(config_dir, filename), "r") as f:
+        param_list.append(json.load(f))
+
+# with open(f"{config_dir}/4.7_single.json", "r") as f:
+#     game_params = json.load(f)
 
 policy_kwargs = dict(
     features_extractor_class=CustomCNN,
@@ -18,16 +24,14 @@ policy_kwargs = dict(
     net_arch=dict(pi=[128, 64], vf=[256, 256, 128])
 )
 
-vec_env = make_vec_env(lambda: SnakeGameEnv(**game_params), n_envs=32)
 # env = SnakeGameEnv(num_snakes=1, num_teams=1)
-
-
-
 # model = PPO('MultiInputPolicy', vec_env, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.0003)
-model = PPO('CnnPolicy', vec_env, policy_kwargs=policy_kwargs,verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.0003)
 # model = PPO.load("ppo_snake4.5_2.zip", env=vec_env, device="cuda", tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.0003)
 # print(model.get_parameters()['policy'].keys())
+for index, config in enumerate(param_list):
+    vec_env = make_vec_env(lambda: SnakeGameEnv(**config), n_envs=32)
+    model = PPO('CnnPolicy', vec_env, policy_kwargs=policy_kwargs,verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.0003)
 
-for i in range(40):
-    model.learn(100000, progress_bar=True, tb_log_name="ppo_snake4.7_single", reset_num_timesteps=False)
-    model.save('ppo_snake4.7_single.zip')
+    for i in range(40):
+        model.learn(100000, progress_bar=True, tb_log_name=f"ppo_snake4.8_{index}", reset_num_timesteps=False)
+        model.save(f'ppo_snake4.8_{index}.zip')
