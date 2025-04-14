f674328fa311ed0e9c1e8748269b91f47d5d5eb8
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-04-15 00:39:45 +0800

    train 4.8_staged_long

--- local_test/staged_learning.py
+++ local_test/staged_learning.py
@@ -30,7 +30,7 @@ for i, params in enumerate(param_list):
         model = PPO('CnnPolicy', vec_env, policy_kwargs=policy_kwargs, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.00025)
         # model = A2C('CnnPolicy', vec_env, policy_kwargs=policy_kwargs, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=128, learning_rate=0.0003)
 
-    num_repeats = 20
+    num_repeats = 40
     for j in range(num_repeats):
-        model.learn(200000, progress_bar=True, tb_log_name=f"{model_name}_{i}", reset_num_timesteps=False)
-        model.save(f'{model_name}_{i}.zip')
+        model.learn(200000, progress_bar=True, tb_log_name=f"{model_name}_long_{i}", reset_num_timesteps=False)
+        model.save(f'{model_name}_long_{i}.zip')
