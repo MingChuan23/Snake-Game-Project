6ed31f343527ead646ce27c25d32cb085f2b2340
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-04-15 02:02:37 +0800

    train 4.8 with 0.01 ent, decaying

--- local_test/staged_learning.py
+++ local_test/staged_learning.py
@@ -12,7 +12,7 @@ config_dir = f"param_configs/{model_name}"
 
 param_list = []
 
-ent_schedule = get_schedule_fn(0.05)
+ent_schedule = get_schedule_fn(0.01)
 
 for filename in sorted(os.listdir(config_dir)):
     with open(os.path.join(config_dir, filename), "r") as f:
@@ -33,7 +33,7 @@ for i, params in enumerate(param_list):
         model = PPO('CnnPolicy', vec_env, policy_kwargs=policy_kwargs, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.00025, ent_coef=ent_schedule)
         # model = A2C('CnnPolicy', vec_env, policy_kwargs=policy_kwargs, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=128, learning_rate=0.0003)
 
-    num_repeats = 40
+    num_repeats = 4
     for j in range(num_repeats):
-        model.learn(200000, progress_bar=True, tb_log_name=f"{model_name}_ent_large_arch_sched{i}", reset_num_timesteps=False)
-        model.save(f'{model_name}_ent_large_arch_sched{i}.zip')
+        model.learn(1000000, progress_bar=True, tb_log_name=f"{model_name}_ent_large_arch_sched2{i}", reset_num_timesteps=False)
+        model.save(f'{model_name}_ent_large_arch_sched2{i}.zip')
