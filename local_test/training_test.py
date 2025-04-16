9051b099e278243ec0b37609a3ff678cc95e48b1
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-04-16 15:16:21 +0800

    train 6gs without customcnn

--- local_test/training_test.py
+++ local_test/training_test.py
@@ -7,7 +7,7 @@ import os
 
 
 log_dir = "logs"
-config_dir = "param_configs/ga_params"
+config_dir = "param_configs"
 
 param_list = []
 
@@ -15,14 +15,14 @@ param_list = []
 #     with open(os.path.join(config_dir, filename), "r") as f:
 #         param_list.append(json.load(f))
 
-with open(f"{config_dir}/d.json", "r") as f:
+with open(f"{config_dir}/4.7_single.json", "r") as f:
     game_params = json.load(f)
 
-policy_kwargs = dict(
-    features_extractor_class=CustomCNN,
-    features_extractor_kwargs=dict(features_dim=256),
-    net_arch=dict(pi=[128, 64], vf=[256, 256, 128])
-)
+# policy_kwargs = dict(
+#     features_extractor_class=CustomCNN,
+#     features_extractor_kwargs=dict(features_dim=256),
+#     net_arch=dict(pi=[128, 64], vf=[256, 256, 128])
+# )
 
 # env = SnakeGameEnv(num_snakes=1, num_teams=1)
 # model = PPO('MultiInputPolicy', vec_env, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.0003)
@@ -30,8 +30,8 @@ policy_kwargs = dict(
 # print(model.get_parameters()['policy'].keys())
 # for index, config in enumerate(param_list):
 vec_env = make_vec_env(lambda: SnakeGameEnv(**game_params), n_envs=32)
-model = PPO('CnnPolicy', vec_env, policy_kwargs=policy_kwargs,verbose=False, device='cuda', tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.0003, gae_lambda=0.99)
+model = PPO('CnnPolicy', vec_env,verbose=False, device='cuda', tensorboard_log=log_dir, n_steps=128, batch_size=2048, learning_rate=0.0003, gae_lambda=0.99)
 
 for i in range(40):
-    model.learn(100000, progress_bar=True, tb_log_name=f"ppo_snake4.8_d2", reset_num_timesteps=False)
-    model.save(f'ppo_snake4.8_d2.zip')
+    model.learn(100000, progress_bar=True, tb_log_name=f"ppo_snake_6gs", reset_num_timesteps=False)
+    model.save(f'ppo_snake_6gs.zip')
